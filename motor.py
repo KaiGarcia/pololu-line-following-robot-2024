5cc9a3ffb8cfd5b580946a9362da7899acd675a6
dyso25 <dylans808@gmail.com>
2024-11-29 14:45:37 -1000

    Added PID function

--- motor.py
+++ motor.py
@@ -1,16 +1,15 @@
 import digitalio
+import analogio
 import pwmio
 import time
-from board import D2, D3, D4, D5, D10, D11, D12, D13
+from board import A6, A7, D2, D3, D4, D5, D10, D11, D12, D13
 
 # Setup for Motor A
-ain1 = digitalio.DigitalInOut(D4)
-ain1.direction = digitalio.Direction.OUTPUT
+ain1 = analogio.AnalogOut(A6)
 pwm_a = pwmio.PWMOut(D2, frequency=1000)
 
 # Setup for Motor B
-bin1 = digitalio.DigitalInOut(D5)
-bin1.direction = digitalio.Direction.OUTPUT
+bin1 = analogio.AnalogOut(A7)
 pwm_b = pwmio.PWMOut(D3, frequency=1000)
 
 # Change the direction pins
@@ -26,24 +25,6 @@ enc_b1 = digitalio.DigitalInOut(D12)
 enc_b1.direction = digitalio.Direction.INPUT
 enc_b2 = digitalio.DigitalInOut(D13)
 enc_b2.direction = digitalio.Direction.INPUT
-
-
-
-# Function that sets the speed for each Motor independently
-def setMotors(dutyA, dutyB):
-    motors = {'A': (ain1, pwm_a, dutyA), 'B': (bin1, pwm_b, dutyB)}
-    
-    for motor, (in1, pwm, dutyCycle) in motors.items():
-        # Set direction and duty cycle
-        if dutyCycle > 0:  # Forward (CW)
-            in1.value = True
-            pwm.duty_cycle = int((dutyCycle / 100) * 65535)
-        elif dutyCycle < 0:  # Backward (CCW)
-            in1.value = False
-            pwm.duty_cycle = int((abs(dutyCycle) / 100) * 65535)
-        else:  # Stop
-            in1.value = False
-            pwm.duty_cycle = 0  # Stop motor
 
 # Function that measures the speed of the encoders and outputs 
 def encoder(time_step):
@@ -80,6 +61,76 @@ def encoder(time_step):
     rpm_B = rotations_B * 60 / time_step
     
     return rpm_A, rpm_B
+
+# Set up error terms for PID tuning
+previous_error = 0
+integral_error = 0
+measureStart = time.monotonic()
+
+# Perform PID tuning
+def pid(setpoint, current):
+    global previous_error, integral_error, measureStart
+
+    # Proportional, Derivative, and Integral Gains
+    kp = 0.12
+    kd = 0.05
+    ki = 0.5
+    feedforward = 40
+
+    error = setpoint - current
+
+    dt = time.monotonic() - measureStart
+    p = kp * error
+    d = kd * (error - previous_error) / dt
+    if integral_error >= 1200:
+        integral_error = 1200
+    i = ki * integral_error
+    
+    # print("Error = ", error)
+    # print("Derivative Error = ", error-previous_error)
+    # print("Integral Error = ", integral_error)
+    previous_error = error
+    measureStart = time.monotonic()
+    integral_error += error
+    update = p + d + i
+    # print(update)
+    updateDuty = int(update*(2**15)/300)
+    return updateDuty
+
+# Function that sets the speed for each Motor independently
+def setMotorsDuty(dutyA, dutyB):
+    motors = {'A': (ain1, pwm_a, dutyA), 'B': (bin1, pwm_b, dutyB)}
+    
+    for motor, (in1, pwm, dutyCycle) in motors.items():
+        # Set direction and duty cycle
+        if dutyCycle > 0:  # Forward (CW)
+            in1.value = 65535
+            pwm.duty_cycle = int((dutyCycle / 100) * 65535)
+        elif dutyCycle < 0:  # Backward (CCW)
+            in1.value = 0
+            pwm.duty_cycle = int((abs(dutyCycle) / 100) * 65535)
+        else:  # Stop
+            in1.value = 0
+            pwm.duty_cycle = 0  # Stop motor
+
+
+def setMotors(speedA, speedB):
+    # Measure the current rpm of Motor A and Motor B
+    calcTime = 0.1 # Time to make each PID update
+    rpmA, rpmB = encoder(calcTime)
+
+    motors = {'A': (ain1, pwm_a, rpmA, speedA), 'B': (bin1, pwm_b, rpmB, speedB)}
+
+    for motor, (in1, pwm, rpm, speed) in motors.items():
+        # Take PID measurement 
+        x = pid(speed, rpm)
+        if x < 0:
+            in1.value = 0
+            pwm.duty_cycle = abs(x)
+        else:
+            in1.value = 65535
+            pwm.duty_cycle = abs(x)
+
 
 while True:
     print(encoder(0.5))
